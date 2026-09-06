2a64de76c5d4c4d15cfd519febed157260324ffc
Linares26 <127995036+Linares26@users.noreply.github.com>
2026-09-06 17:27:15 +0100

    feat: initialize Django settings, add database/static management dependencies, and include project requirement files

--- sos_nfc_3dimensiapt/sos_nfc_3dimensiapt/settings.py
+++ sos_nfc_3dimensiapt/sos_nfc_3dimensiapt/settings.py
@@ -4,6 +4,7 @@ Archivo: sos_nfc_3dimensiapt/settings.py
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -59,12 +60,23 @@ WSGI_APPLICATION = 'sos_nfc_3dimensiapt.wsgi.application'
 # ==========================================
 # BASE DE DATOS
 # ==========================================
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/tmp/db.sqlite3',
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # ==========================================
 # ARCHIVOS ESTÁTICOS Y MULTIMEDIA (FOTOS)
