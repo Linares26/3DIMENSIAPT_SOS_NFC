3de23ac329b68624381cf489964dad64834e4aa9
Linares26 <127995036+Linares26@users.noreply.github.com>
2026-09-06 18:03:18 +0100

    Agregar whitenoise para archivos staticos en Vercel

--- sos_nfc_3dimensiapt/sos_nfc_3dimensiapt/settings.py
+++ sos_nfc_3dimensiapt/sos_nfc_3dimensiapt/settings.py
@@ -82,7 +82,8 @@ else:
 # ARCHIVOS ESTÁTICOS Y MULTIMEDIA (FOTOS)
 # ==========================================
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
