ce2ba00672cde876546b19aa817d817b6595da7d
Linares26 <127995036+Linares26@users.noreply.github.com>
2026-09-06 15:47:13 +0100

    Configuracion para Vercel

--- sos_nfc_3dimensiapt/sos_nfc_3dimensiapt/settings.py
+++ sos_nfc_3dimensiapt/sos_nfc_3dimensiapt/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-3dimensiapt-nfc-key')
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.3dimensiapt.com', '3dimensiapt.com', 'localhost', '127.0.0.1']
 
 # APPS INSTALADAS
 INSTALLED_APPS = [
