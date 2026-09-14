5288997991794c18390a3656e313af3fc2829684
Linares26 <127995036+Linares26@users.noreply.github.com>
2026-09-14 20:46:39 +0100

    update media

--- sos_nfc_3dimensiapt/sos_nfc_3dimensiapt/settings.py
+++ sos_nfc_3dimensiapt/sos_nfc_3dimensiapt/settings.py
@@ -109,15 +109,15 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = BASE_DIR / 'media'
 
 # --- ALMACENAMIENTO DE ARCHIVOS MEDIA (SUPABASE STORAGE) ---
 AWS_ACCESS_KEY_ID = os.environ.get('SUPABASE_S3_ACCESS_KEY')
 AWS_SECRET_ACCESS_KEY = os.environ.get('SUPABASE_S3_SECRET_KEY')
 AWS_STORAGE_BUCKET_NAME = 'media'
 AWS_S3_ENDPOINT_URL = os.environ.get('SUPABASE_S3_ENDPOINT')
-AWS_S3_REGION_NAME = 'us-east-1'  # Región genérica requerida por boto3
+AWS_S3_REGION_NAME = 'eu-west-1'  # Región genérica requerida por boto3
 AWS_S3_FILE_OVERWRITE = False
 
 if AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY:
